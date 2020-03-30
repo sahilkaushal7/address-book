53160040561f2d311f926ddb7de71487eb7a5a27
Sahil <skaushal@breadware.com>
2020-03-30 21:56:38 +0530

    adding bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
